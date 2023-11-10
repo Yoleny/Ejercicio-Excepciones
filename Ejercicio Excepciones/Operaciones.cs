32a96a615493d958ca99d4e5346c84d6ca092452
Yoleny <yoleny.salme28@gmail.com>
2023-11-10 16:24:34 -0600

    Se agrego la clase operaciones

---
+++ Ejercicio Excepciones/Operaciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Excepciones
+{
+    class Operaciones
+    {
+        public static void Sumar(double a, double b)
+        {
+            double resultado = a + b;
+            Console.WriteLine("La suma de {0} y {1} es: {2}", a, b, resultado);
+        }
+
+        public static void Restar(double a, double b)
+        {
+            double resultado = a - b;
+            Console.WriteLine("La resta de {0} y {1} es: {2}", a, b, resultado);
+        }
+
+        public static void Multiplicar(double a, double b)
+        {
+            double resultado = a * b;
+            Console.WriteLine("La multiplicacion de {0} y {1} es: {2}", a, b, resultado);
+        }
+
+
+        public static void Dividir(double a, double b)
+        {
+            double resultado = a / b;
+            Console.WriteLine("La división de {0} entre {1} es: {2}", a, b, resultado);
+        }
+    }
+}
--- Ejercicio Excepciones/Program.cs
+++ Ejercicio Excepciones/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Ejercicio_Excepciones;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el primer número: ");
+        string numero1 = Console.ReadLine();
+
+        Console.WriteLine("Ingrese el segundo número: ");
+        string numero2 = Console.ReadLine();
+
+        try
+        {
+            double a = Convert.ToDouble(numero1);
+            double b = Convert.ToDouble(numero2);
+
+            Operaciones.Sumar(a, b);
+            Operaciones.Restar(a, b);
+            Operaciones.Multiplicar(a, b);
+            Operaciones.Dividir(a, b);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Debe ingresar números válidos.");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: No se puede dividir entre cero.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: {0}", ex.Message);
+        }
+    }
+}
